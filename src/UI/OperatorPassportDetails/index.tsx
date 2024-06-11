7a430650316081779bf4fb82ff962b4d8ef3707e
Bahriddin <bahridnurullav@gmail.com>
2024-06-11 07:54:37 +0500

    deployment error

--- src/UI/OperatorPassportDetails/index.tsx
+++ src/UI/OperatorPassportDetails/index.tsx
@@ -96,18 +96,18 @@ function PassportDetails() {
     });
   };
 
-  const notifyError = (text: string) => {
-    toast.error(`🦄 ${text}`, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "colored",
-    });
-  };
+  // const notifyError = (text: string) => {
+  //   toast.error(`🦄 ${text}`, {
+  //     position: "top-right",
+  //     autoClose: 5000,
+  //     hideProgressBar: false,
+  //     closeOnClick: true,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     progress: undefined,
+  //     theme: "colored",
+  //   });
+  // };
 
   return (
     <div style={{textAlign: "right", position: "relative"}}>
