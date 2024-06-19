44e46c16794e06e6c51cd04fbd43f0de292f3ebd
Bahriddin <bahridnurullav@gmail.com>
2024-06-19 09:49:44 +0500

    login is connected

--- src/UI/ADMIN/AdminLogin/index.tsx
+++ src/UI/ADMIN/AdminLogin/index.tsx
@@ -72,18 +72,18 @@ function AdminLogin() {
     });
   };
 
-  const notifyError = (text: string) => {
-    toast.error(`${text}`, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "colored",
-    });
-  };
+  // const notifyError = (text: string) => {
+  //   toast.error(`${text}`, {
+  //     position: "top-right",
+  //     autoClose: 5000,
+  //     hideProgressBar: false,
+  //     closeOnClick: true,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     progress: undefined,
+  //     theme: "colored",
+  //   });
+  // };
 
   return (
     <div className={styles.loginForm}>
